165174506839f1572056ce34f35cb63935bac679
Johan Vergeer <johan.vergeer@ordina.nl>
2020-02-17 11:28:08 +0100

    Make test return types explicit

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -5,46 +5,46 @@ import pytest
 from fibonacci import fibonacci_sequence, fibonacci_up_to, fibonacci_value
 
 
-def test_fibonacci_value__0():
+def test_fibonacci_value__0() -> None:
     assert fibonacci_value(0) == 0
 
 
-def test_fibonacci_value__1():
+def test_fibonacci_value__1() -> None:
     assert fibonacci_value(1) == 1
 
 
-def test_fibonacci_value__75():
+def test_fibonacci_value__75() -> None:
     assert fibonacci_value(75) == 2111485077978055
 
 
-def test_fibonacci_sequence__values_count_0():
+def test_fibonacci_sequence__values_count_0() -> None:
     with pytest.raises(ValueError) as e:
         fibonacci_sequence(0)
 
     assert "values_count must be greater than 0" in str(e.value)
 
 
-def test_fibonacci_sequence__values_count__smaller_than_0():
+def test_fibonacci_sequence__values_count__smaller_than_0() -> None:
     with pytest.raises(ValueError) as e:
         fibonacci_sequence(-1)
 
     assert "values_count must be greater than 0" in str(e.value)
 
 
-def test_fibonacci_sequence__values_count__1():
+def test_fibonacci_sequence__values_count__1() -> None:
     assert fibonacci_sequence(1) == [0]
 
 
-def test_fibonacci_sequence__values_count__2():
+def test_fibonacci_sequence__values_count__2() -> None:
     assert fibonacci_sequence(2) == [0, 1]
     assert fibonacci_sequence(1) == [0]
 
 
-def test_fibonacci_sequence__values_count__3():
+def test_fibonacci_sequence__values_count__3() -> None:
     assert fibonacci_sequence(3) == [0, 1, 1]
 
 
-def test_fibonacci_sequence__values_count__4():
+def test_fibonacci_sequence__values_count__4() -> None:
     assert fibonacci_sequence(4) == [0, 1, 1, 2]
 
 
